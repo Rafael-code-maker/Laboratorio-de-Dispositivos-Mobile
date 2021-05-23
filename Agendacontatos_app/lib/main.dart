c2b3690297471bc72f5d2288f7d986a933daa9ce
Rafael Maia <rafaeldepaulamaia@yahoo.com.br>
2021-05-23 16:32:10 -0300

    Add files via upload

---
+++ Agendacontatos_app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:agendacontatos_app/PaginaLogin.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:agendacontatos_app/home.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Agenda de Contatos',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: PaginaLogin(),
+    );
+  }
+}
